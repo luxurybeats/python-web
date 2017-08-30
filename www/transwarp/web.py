d0af20fb59659bf30cea8cdb43b510ae57c556fa
Luxury <lx644966783@sina.com>
2017-08-30 21:21:27 +0800

    day8 web frame over

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -1017,21 +1017,149 @@ def _default_error_handler(e, start_response, is_debug):
         return _debug()
     return ('<html><body><h1>500 Internal Server Error</h1><h3>%s</h3></body></html>' % str(e))
 
-def interceptor(pattern):
+#################################################################
+# 实现URL拦截器
+# 主要interceptor的实现
+#################################################################
+
+_RE_INTERCEPTOR_STARTS_WITH = re.compile(r'^([^\*\?]+)\*?$')
+_RE_INTERCEPTOR_END_WITH = re.compile(r'^\*([^\*\?]+)$')
+
+def _build_pattern_fn(pattern):
+    """
+    传入需要匹配的字符串： URL
+    返回一个函数，该函数接收一个字符串参数，检测该字符串是否
+    符合pattern
+    :param pattern:
+    :return:
+    """
+    m = _RE_INTERCEPTOR_STARTS_WITH.match(pattern)
+    if m:
+        return lambda p: p.startwith(m.group(1))
+    m = _RE_INTERCEPTOR_END_WITH.match(pattern)
+    if m:
+        return lambda p: p.endwith(m.group(1))
+    raise ValueError('Invalid pattern definition in interceptor.')
+
+def interceptor(pattern='/'):
     """
     定义拦截器
     :param pattern:
     :return:
     """
+    def _decorator(func):
+        func.__interceptor__ = _build_pattern_fn(pattern)
+        return func
+    return _decorator
+
+def _build_interceptor_fn(func, next):
+    """
+    拦截器接受一个next函数，这样，一个拦截器可以决定调用next()继续处理请求还是直接返回
+    :param func:
+    :param next:
+    :return:
+    """
+    def _wrapper():
+        if func.__interceptor__(ctx.request.path_info):
+            return func(next)
+        else:
+            return next()
+    return _wrapper
+
+def _build_interceptor_chain(last_fn, *interceptors):
+    """
+     Build interceptor chain.
+    :param last_fn:
+    :param interceptors:
+    :return:
+    """
+    L = list(interceptors)
+    L.reverse()                                 # 对list 反向排序
+    fn = last_fn
+    for f in L:
+        fn = _build_interceptor_fn(f, fn)
+    return fn
+
+def _load_module(module_name):
+    """
+      Load module from name as str.
+    :param module_name:
+    :return:
+    """
+    last_dot = module_name.rfind('.')       #Python rfind() 返回字符串最后一次出现的位置(从右向左查询)，如果没有匹配项则返回-1。
+    if last_dot == (-1):
+        return __import__(module_name, globals(), locals())
+    from_module = module_name[:last_dot]
+    import_module = module_name[last_dot+1:]
+    m = __import__(from_module, globals(), locals(), [import_module])
+    print m
+    return getattr(m, import_module)
+
+#################################################################
+# 全局WSGIApplication的类，实现WSGI接口
+# WSGIApplication 封装了 wsgi Server（run方法） 和 wsgi 处理函数（wsgi静态方法）
+# 上面的所有的功能都是对 wsgi 处理函数的装饰
+#################################################################
+
+def badrequest():
     pass
 
 
 class WSGIApplication(object):
     """
-
+    WSBIApplication 应用接口
     """
     def __init__(self, document_root=None, **kw):
-        pass
+        self._running = False
+        self._document_root = document_root
+
+        self._interceptors = []
+        self._template_engine = None
+
+        self._get_static = {}
+        self._post_static = {}
+
+        self._get_dynamic = []
+        self._post_dynamic = []
+
+    def _check_not_running(self):
+        """
+        检测app对象 是否运行
+        :return:
+        """
+        if self._running:
+            raise RuntimeError('Cannot modify WSGIApplication when running.')
+    @property
+    def template_engine(self):
+        """
+        设置TemplateEngine
+        :return:
+        """
+        return self._template_engine
+
+    @template_engine.setter
+    def template_engine(self, engine):
+        """
+        设置app 使用的模板引擎
+        :param engine:
+        :return:
+        """
+        self._check_not_running()
+        self._template_engine = engine
+
+    def add_module(self, mod):
+        """
+
+        :param mod:
+        :return:
+        """
+        self._check_not_running()
+        m = mod if type(mod) == types.ModuleType else _load_module(mod)
+        logging.info('Add module: %s' % m.__name__)
+        for name in dir(m):
+            fn = getattr(m, name)
+            if callable(fn) and hasattr(fn, '__web_route__') and hasattr(fn, '__web_methd'):
+                self.add_url(fn)
 
     def add_url(self,func):
         """
@@ -1039,7 +1167,19 @@ class WSGIApplication(object):
         :param func:
         :return:
         """
-        pass
+        self._check_not_running()
+        route = Route(func)
+        if route.is_static:
+            if route.method == 'GET':
+                self._get_static[route.path] = route
+            if route.method == 'POST':
+                self._post_static[route.path] = route
+        else:
+            if route.method == 'GET':
+                self._get_dynamic.append(route)
+            if route.method == 'POST':
+                self._post_dynamic.append(route)
+        logging.info('Add route: %s' % str(func))
 
     def add_interceptor(self, func):
         """
@@ -1047,33 +1187,113 @@ class WSGIApplication(object):
         :param func:
         :return:
         """
-        pass
+        self._check_not_running()
+        self._interceptors.append(func)
+        logging.info('Add interceptor: %s' % str(func))
 
-    @property
-    def template_engine(self):
+    def run(self, port=9000, host='127.0.0.1'):
         """
-        设置TemplateEngine
+        启动python自带的WSGI Server
+        :param port:
+        :param host:
         :return:
         """
-        pass
+        from wsgiref.simple_server import make_server
+        logging.info('application (%s) will start at %s:%s...' % (self._document_root, host, port))
+        server = make_server(host, port, self.get_wsgi_application(debug=True))
+        server.server_forever()
 
-    @template_engine.setter
-    def template_engine(self, engine):
-        pass
-
-    def get_wsgi_application(self):
+    def get_wsgi_application(self, debug=False):
         """
         返回WSGI处理函数
         :return:
         """
+        self._check_not_running()
+        if debug:
+            self._get_dynamic.append(StaticFileRoute())
+        self._running = True
+
+        _application = Dict(document_root=self._document_root)
+
+        def fn_route():
+            request_method = ctx.request.request_method
+            path_info = ctx.request.path_info
+            if request_method == 'GET':
+                fn = self._get_static.get(path_info, None)
+                if fn:
+                    return fn()
+                for fn in self._get_dynamic:
+                    args = fn.match(path_info)
+                    if args:
+                        return fn(*args)
+                raise HttpError.notfound()
+            if request_method=='POST':
+                fn = self._post_static.get(path_info, None)
+                if fn:
+                    return fn()
+                for fn in self._post_dynamic:
+                    args = fn.match(path_info)
+                    if args:
+                        return fn(*args)
+                raise HttpError.notfound()
+            raise badrequest()
+
+        fn_exec = _build_interceptor_chain(fn_route(), *self._interceptors)
+
         def wsgi(env, start_response):
-            pass
+            """
+            WSGI 处理函数
+            :param env:
+            :param start_response:
+            :return:
+            """
+            ctx.application = _application
+            ctx.request = Request(env)
+            response = ctx.response = Response()
+            try:
+                r = fn_exec()
+                if isinstance(r, Template):
+                    r = self._template_engine(r.template_name, r.model)
+                if isinstance(r, unicode):
+                    r = r.encode('utf-8')
+                if r is None:
+                    r = []
+                start_response(response.status, response.headers)
+                return r
+            except _RedirectError, e:
+                response.set_header('Location', e.location)
+                start_response(e.status, response.headers)
+                return []
+            except _HttpError, e:
+                start_response(e.status, response.headers)
+                return ['<html><body><h1>', e.status, '</h1></body></html>']
+            except Exception, e:
+                logging.exception(e)
+                if not debug:
+                    start_response('500 Internal Server Error', [])
+                    return ['<html><body><h1>500 Internal Server Error</h1></body></html>']
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                fp = StringIO()
+                traceback.print_exception(exc_type, exc_value, exc_traceback, file=fp)
+                stacks = fp.getvalue()
+                fp.close()
+                start_response('500 Internal Server Error', [])
+                return [
+                    r'''<html><body><h1>500 Internal Server Error</h1><div style="font-family:Monaco, Menlo, Consolas, 'Courier New', monospace;"><pre>''',
+                    stacks.replace('<', '&lt;').replace('>', '&gt;'),
+                    '</pre></div></body></html>']
+            finally:
+                del ctx.application
+                del ctx.request
+                del ctx.response
+
         return wsgi
 
-    def run(self, port=9000, host='127.0.0.1'):
-        from wsgiref.simple_server import make_server
-        server = make_server(host, port, self.get_wsgi_application())
-        server.server_forever()
+if __name__ == '__main__':
+    sys.path.append('.')
+    import doctest
+    doctest.testmod()
+
 
 
 
